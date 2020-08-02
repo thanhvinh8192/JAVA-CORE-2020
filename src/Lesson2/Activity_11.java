69a15e2b5a0672ae2ea55610dd95f8a8089769e3
thanhvinh8192 <thanhvinh8192@gmail.com>
2020-08-02 22:54:15 +0900

    update lesson2 Activity11

--- src/Lesson2/Activity_11.java
+++ src/Lesson2/Activity_11.java
@@ -1,8 +1,19 @@
 package Lesson2;
 
+import java.util.Scanner;
+
 public class Activity_11 {
     public static void main(String[] args) {
         System.out.println("This is my first program");
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter value of a: ");
+        int a = scanner.nextInt();
+        System.out.print("Enter value of b: ");
+        int b = scanner.nextInt();
+        System.out.println("Result of a+b =: " + (a+b));
+        System.out.println("Result of a-b =: " + (a-b));
+        System.out.println("Result of a*b =: " + (a*b));
+        System.out.println("Result of a/b =: " + (a/b));
+        System.out.println("Result of a%+b =: " + (a%b));
     }
 }
