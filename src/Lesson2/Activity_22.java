9d970425e0cbe39a58b9eab0809c701e5a226efc
thanhvinh8192 <thanhvinh8192@gmail.com>
2020-08-05 22:30:32 +0900

    Lesson2 update git

--- src/Lesson2/Activity_22.java
+++ src/Lesson2/Activity_22.java
@@ -7,7 +7,16 @@ public class Activity_22 {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter your number: ");
         int n = scanner.nextInt();
-        int sum = n+ (n*11) + (n*111);
-        System.out.println(n+"+"+n*11+"+"+n*111+"="+sum);
+        if (n<10){
+            int sum = n+ (n*11) + (n*111);
+            System.out.println(n+"+"+n*11+"+"+n*111+"="+sum);
+        }
+        else
+            if(n>10&&n<100){
+                int sum1 = n+ (n*101) + (n*10101);
+                System.out.println(n+"+"+n*101+"+"+n*10101+"="+sum1);
+        }
+            else
+                System.out.println("Your index must be 0<1<100");
     }
 }
