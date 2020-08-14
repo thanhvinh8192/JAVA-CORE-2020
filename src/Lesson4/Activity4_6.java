d2442b37a37af0ee71152d2afaff354aafe6f9b8
thanhvinh8192 <thanhvinh8192@gmail.com>
2020-08-14 17:52:21 +0900

    Lesson4 update final

--- src/Lesson4/Activity4_6.java
+++ src/Lesson4/Activity4_6.java
@@ -1,16 +1,22 @@
 package Lesson4;
 
+import javax.swing.plaf.synth.SynthOptionPaneUI;
 import java.util.Scanner;
 
 public class Activity4_6 {
     public static void main(String[] args) {
         System.out.print("Input the number you want convert: ");
         Scanner scanner = new Scanner(System.in);
-        int numberConvert = scanner.nextInt();
-        for (int i = numberConvert; i>=1; i = i%2){
-            if (i==1){
-                break;
-            }
+        int  decimal = scanner.nextInt();
+        int[] binary = new int[50];
+        int index = 0;
+        while (decimal >0 ){
+            binary[index++] = decimal%2;
+            decimal = decimal/2;
+        }
+        System.out.print("The number after convert to binary: ");
+        for (int i = index-1; i >= 0; i--){
+            System.out.print(binary[i]);
         }
     }
 }
